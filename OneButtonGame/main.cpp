7dc4cf79ca247805cebaffe117254a5f4c7872e3
Mathieu Bardot <bardotmathieu@gmail.com>
2021-01-28 17:31:06 +0100

    Movement modified and add text for scoring

--- OneButtonGame/main.cpp
+++ OneButtonGame/main.cpp
@@ -1,6 +1,7 @@
 #include <SFML/Graphics.hpp>
 #include <math.h>
 #include <cstdlib>
+#include <iostream>
 
 #include "Player.h"
 #include "Obstacle.h"
@@ -13,10 +14,28 @@ int main()
     RenderWindow window(VideoMode(800, 600), "One Button Game");
     window.setFramerateLimit(60);
 
+    //Init text
+    sf::Font font;
+    font.loadFromFile("arial.ttf");
+
+    //UI Init
+    Text score;
+    score.setFont(font);
+    score.setCharacterSize(20);
+    score.setFillColor(Color::White);
+    score.setPosition(10.f, 10.f);
+
+    Text gameOver;
+    gameOver.setFont(font);
+    gameOver.setCharacterSize(50);
+    gameOver.setFillColor(Color::Red);
+    gameOver.setPosition(100.f, (float)(window.getSize().x / 2));
+    gameOver.setString("GAME OVER !");
+
     //Player Init
     Player player(window.getSize());
     int ShootTimer = 20;
-    //int scoring = 0;
+    int scoring = 0;
 
     //Obstacle Init
     int obstacleSpawnTimer = 0;
@@ -31,51 +50,94 @@ int main()
                 window.close();
         }
 
-        //Player movement and collision with window
-        //Movement left and collision with left border 
-        player.shape.move(-10.f, 0.0f);
-        if (player.shape.getPosition().x <= player.shape.getGlobalBounds().width)
-            player.shape.setPosition(0.f, player.shape.getPosition().y); 
-        
-        //Movement right and collision with right border 
-        player.shape.move(10.f, 0.0f);
-        if (player.shape.getPosition().x >= window.getSize().x - player.shape.getGlobalBounds().width) 
-            player.shape.setPosition(window.getSize().x - player.shape.getGlobalBounds().width, player.shape.getPosition().y);
-        
-        //Stop movement
-        if (Keyboard::isKeyPressed(Keyboard::Space))
-            player.shape.setPosition(player.shape.getPosition().x , player.shape.getPosition().y);
-
-        //Enemy
-        if (obstacleSpawnTimer < 40)
-            obstacleSpawnTimer++;
-
-        if (obstacleSpawnTimer >= 40)
+        if (player.getHP() > 0)
         {
-            obstacles.push_back(Obstacle(window.getSize()));
-            obstacleSpawnTimer = 0; //reset timer
-        }
+            //Player movement and collision with window
+            auto borderLeft = player.shape.getGlobalBounds().width;
+            auto borderRight = window.getSize().x - player.shape.getGlobalBounds().width;
 
-        for (size_t i = 0; i < obstacles.size(); i++)
-        {
-            //Movement
-            obstacles[i].shape.move(0.f, 5.0f);
-
-            //Enemy get out of the window
-            if (obstacles[i].shape.getPosition().y <= 0 - obstacles[i].shape.getGlobalBounds().height)
+            if ((player.shape.getPosition().x != borderLeft) && (player.shape.getPosition().x != borderRight))
             {
-                obstacles.erase(obstacles.begin() + i);
-                break;
+                player.shape.move(-10.f, 0.0f);
+                std::cout << "Move left !" << std::endl;
+
+                if (player.shape.getPosition().x <= player.shape.getGlobalBounds().width)
+                {
+                    std::cout << "Collision !" << std::endl;
+                    player.shape.setPosition(0.f, player.shape.getPosition().y);
+                }
+                player.shape.move(0.0f, 0.0f);
+                std::cout << "Stop move !" << std::endl;
+
+                if ((player.shape.getPosition().x == borderLeft))
+                {
+                    player.shape.move(10.f, 0.0f);
+                    std::cout << "Move Right !" << std::endl;
+
+                    if (player.shape.getPosition().x >= window.getSize().x - player.shape.getGlobalBounds().width)
+                    {
+                        player.shape.setPosition(window.getSize().x - player.shape.getGlobalBounds().width, player.shape.getPosition().y);
+                        std::cout << "Collision !" << std::endl;
+                    }
+                    player.shape.move(0.0f, 0.0f);
+                    std::cout << "Stop move !" << std::endl;
+                }
             }
 
-            //Enemy collision with Player
-            if (obstacles[i].shape.getGlobalBounds().intersects(player.shape.getGlobalBounds()))
+            
+
+            //Movement left and collision with left border 
+            /*player.shape.move(-10.f, 0.0f);
+            if (player.shape.getPosition().x <= player.shape.getGlobalBounds().width)
             {
-                obstacles.erase(obstacles.begin() + i);
-                player.setHP(player.getHP() - obstacles[i].getHP());
-                break;
+                player.shape.setPosition(0.f, player.shape.getPosition().y);
+            }*/
+
+            //Movement right and collision with right border 
+            /*player.shape.move(10.0f, 0.0f);
+            if (player.shape.getPosition().x >= window.getSize().x - player.shape.getGlobalBounds().width)
+            {
+                player.shape.setPosition(window.getSize().x - player.shape.getGlobalBounds().width, player.shape.getPosition().y);
+            }*/
+
+            //Stop movement
+            if (Keyboard::isKeyPressed(Keyboard::Space))
+                player.shape.setPosition(player.shape.getPosition().x, player.shape.getPosition().y);
+
+            //Enemy
+            if (obstacleSpawnTimer < 40)
+                obstacleSpawnTimer++;
+
+            if (obstacleSpawnTimer >= 40)
+            {
+                obstacles.push_back(Obstacle(window.getSize()));
+                obstacleSpawnTimer = 0; //reset timer
+            }
+
+            for (size_t i = 0; i < obstacles.size(); i++)
+            {
+                //Movement
+                obstacles[i].shape.move(0.f, 5.0f);
+
+                //Enemy get out of the window
+                if (obstacles[i].shape.getPosition().y <= 0 - obstacles[i].shape.getGlobalBounds().height)
+                {
+                    obstacles.erase(obstacles.begin() + i);
+                    break;
+                }
+
+                //Enemy collision with Player
+                if (obstacles[i].shape.getGlobalBounds().intersects(player.shape.getGlobalBounds()))
+                {
+                    obstacles.erase(obstacles.begin() + i);
+                    player.setHP(player.getHP() - obstacles[i].getHP());
+                    break;
+                }
             }
         }
+
+        // UI Update
+        score.setString("Score :" + std::to_string(scoring));
 
         window.clear();
         window.draw(player.shape);
@@ -84,6 +146,12 @@ int main()
         {
             window.draw(obstacles[i].shape);
         }
+
+        // UI
+        window.draw(score);
+
+        if (player.getHP() <= 0)
+            window.draw(gameOver);
 
         window.display();
     }
